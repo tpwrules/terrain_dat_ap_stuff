313acf7e31ccd8989473e61f0be85cd7454f0f99
Thomas Watson <twatson52@icloud.com>
2026-01-25 12:09:38 -0600

    stuff to dump coordinate errors we didn't really use

--- terraingen/terrain_gen.py
+++ terraingen/terrain_gen.py
@@ -324,3 +324,41 @@ def create_degree(downloader, lat, lon, folder, grid_spacing, format):
     dfile.finalise()
     return True
 
+def create_degree_map(lat, lon, grid_spacing, format):
+    '''create data file for one degree lat/lon'''
+    lat_int = int(math.floor(lat))
+    lon_int = int(math.floor((lon)))
+
+    latlon_e7_vals = []
+    meter_vals = []
+    error_vals = []
+
+    blocknum = -1
+
+    while True:
+        blocknum += 1
+        (lat_e7, lon_e7) = pos_from_file_offset(lat_int, lon_int, blocknum * IO_BLOCK_SIZE, grid_spacing, format)
+        if lat_e7*1.0e-7 - lat_int >= 1.0:
+            break
+        lat = lat_e7 * 1.0e-7
+        lon = lon_e7 * 1.0e-7
+        grid = GridBlock(lat_int, lon_int, lat, lon, grid_spacing, format)
+        if grid.blocknum() != blocknum:
+            print("what??")
+            continue
+        for gx in range(TERRAIN_GRID_BLOCK_SIZE_X):
+            for gy in range(TERRAIN_GRID_BLOCK_SIZE_Y):
+                lat_e7, lon_e7 = add_offset(lat*1.0e7, lon*1.0e7, gx*grid_spacing, gy*grid_spacing, format)
+                
+                # this is a lot closer to what AP does in calculate_grid_info
+                mx = (grid.grid_idx_x*TERRAIN_GRID_BLOCK_SPACING_X+gx)*grid_spacing
+                my = (grid.grid_idx_y*TERRAIN_GRID_BLOCK_SPACING_Y+gy)*grid_spacing
+
+                lat2, lon2 = add_offset(lat_int*1e7, lon_int*1e7, mx, my, format)
+
+                error_vals.append((abs(lat_e7-lat2), abs(lon_e7-lon2)))
+
+                latlon_e7_vals.append((lat_e7, lon_e7))
+                meter_vals.append((mx, my))
+
+    return latlon_e7_vals, meter_vals, error_vals
---
+++ terraingen/testmap.py
@@ -0,0 +1,5 @@
+from terrain_gen import create_degree_map
+
+latlon_e7_vals, meter_vals, error_vals = create_degree_map(35, -90, 30, "4.1")
+
+breakpoint()
