e66b768f28ef5c3a23b111edab5ffaca27b1171b
Thomas Watson <twatson52@icloud.com>
2026-01-25 10:47:59 -0600

    initial dev stuff

---
+++ flake.nix
@@ -0,0 +1,52 @@
+# HOW TO USE:
+# 1. make a `profiles/` directory in your repo and add to .gitignore
+# 2. nix develop --profile profiles/dev
+# 3. done!
+#
+# the shell is completely safe from garbage collection and evaluates instantly
+# due to Nix's native caching. if you want logs during build, add `-L` to 
+# `nix develop`.
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }: let
+    inputs = { inherit nixpkgs; };
+    system = "x86_64-linux";
+
+    pkgs = nixpkgs.legacyPackages."${system}";
+
+    # or, if you need to add an overlay:
+    # pkgs = import nixpkgs {
+    #   inherit system;
+    #   overlays = [
+    #     (import ./nix/overlay.nix)
+    #   ];
+    # };
+
+    # a text file containing the paths to the flake inputs in order to stop
+    # them from being garbage collected
+    pleaseKeepMyInputs = pkgs.writeTextDir "bin/.please-keep-my-inputs"
+      (builtins.concatStringsSep " " (builtins.attrValues inputs));
+  in {
+    devShell."${system}" = pkgs.mkShellNoCC {
+      buildInputs = [
+        # needed for version info and such
+        pkgs.git
+
+        # used for SITL (console and map modules must be manually loaded)
+        pkgs.mavproxy
+
+        pkgs.gdal
+        pkgs.proj
+
+        (pkgs.python3.withPackages (p: [
+          p.numpy
+          p.matplotlib
+          p.pyproj
+        ]))
+
+        pleaseKeepMyInputs
+      ];
+    };
+  };
+}
