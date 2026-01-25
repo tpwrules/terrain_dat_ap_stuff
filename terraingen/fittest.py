c76fca115812171e5ff99f153b9ec9b0db86bb49
Thomas Watson <twatson52@icloud.com>
2026-01-25 13:52:22 -0600

    test the stackoverflow

---
+++ terraingen/fittest.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# https://stackoverflow.com/a/57923405
+def polyfit2d(x, y, z, kx=3, ky=3, order=None):
+    '''
+    Two dimensional polynomial fitting by least squares.
+    Fits the functional form f(x,y) = z.
+
+    Notes
+    -----
+    Resultant fit can be plotted with:
+    np.polynomial.polynomial.polygrid2d(x, y, soln.reshape((kx+1, ky+1)))
+
+    Parameters
+    ----------
+    x, y: array-like, 1d
+        x and y coordinates.
+    z: np.ndarray, 2d
+        Surface to fit.
+    kx, ky: int, default is 3
+        Polynomial order in x and y, respectively.
+    order: int or None, default is None
+        If None, all coefficients up to maxiumum kx, ky, ie. up to and including x^kx*y^ky, are considered.
+        If int, coefficients up to a maximum of kx+ky <= order are considered.
+
+    Returns
+    -------
+    Return paramters from np.linalg.lstsq.
+
+    soln: np.ndarray
+        Array of polynomial coefficients.
+    residuals: np.ndarray
+    rank: int
+    s: np.ndarray
+
+    '''
+
+    # # grid coords
+    x, y = np.meshgrid(x, y)
+    # coefficient array, up to x^kx, y^ky
+    coeffs = np.ones((kx+1, ky+1))
+
+    # solve array
+    a = np.zeros((coeffs.size, x.size))
+
+    # for each coefficient produce array x^i, y^j
+    for index, (j, i) in enumerate(np.ndindex(coeffs.shape)):
+        # do not include powers greater than order
+        if order is not None and i + j > order:
+            arr = np.zeros_like(x)
+        else:
+            arr = coeffs[i, j] * x**i * y**j
+        a[index] = arr.ravel()
+
+    # do leastsq fitting and return leastsq result
+    return np.linalg.lstsq(a.T, np.ravel(z), rcond=None)
+
+gx = np.asarray(range(3))
+gy = np.asarray(range(2))
+gv = np.asarray(([0, 1, 2], [1, 2, 3]))
+
+f, *_ = polyfit2d(gx, gy, gv, order=1)
+
+r = np.polynomial.polynomial.polygrid2d(gx, gy, f.reshape(4, 4))
+print(gv.shape, r.shape) # why don't these match
+
+plt.matshow(r)
+plt.show()
